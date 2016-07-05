31ce4e2a2613a604bfcc6fb6b725566a56f28fd3
Sarah <sarahkane682@yahoo.com>
2016-07-05 15:16:19 -0700

    we can move tiles up down left and right

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,21 +1,34 @@
 var Game = function() {
   // Game logic and initialization here
+
 };
+
+
+Game.prototype.newTile = function(newTile){
+
+return $('#gameboard').append("<div class='tile' data-row='r" + ( Math.floor(Math.random() * 4) + 1) + "', data-col='c" + ( Math.floor(Math.random() * 4) + 1) + "' data-val='2'>2</div>")
+}
+
+
 
 Game.prototype.moveTile = function(tile, direction) {
   // Game method here
   switch(direction) {
     case 38: //up
       console.log('up');
+      $('.tile').attr("data-row", "r0")
       break;
     case 40: //down
       console.log('down');
+        $('.tile').attr("data-row", "r3")
       break;
     case 37: //left
       console.log('left');
+        $('.tile').attr("data-col", "c0")
       break;
     case 39: //right
       console.log('right');
+        $('.tile').attr("data-col", "c3")
       break;
   }
 };
@@ -34,6 +47,7 @@ $(document).ready(function() {
     }
   });
     $('body').keyup(function(event){
-      var tile_div = $('#gameboard').append("<div class='tile' data-row='r1', data-col='c3' data-val='2'>2</div>")
+      game.newTile()
     })
+
 });
