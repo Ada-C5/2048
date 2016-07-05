ddff0641fa9a03391bf8427603569df026135a93
sophia <sophiabaldonado@gmail.com>
2016-07-05 14:56:47 -0700

    Fix moveTile and rm semicolons;

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,21 +1,23 @@
-var Game = function() {
+var Game = function () {
   // Game logic and initialization here
-};
 
-Game.prototype.moveTile = function(tile, direction) {
+
+}
+
+Game.prototype.moveTile = function (tile, direction) {
   // Game method here
   switch(direction) {
     case 38: //up
-      console.log('up');
+      console.log('up')
       break;
     case 40: //down
-      console.log('down');
+      console.log('down')
       break;
     case 37: //left
-      console.log('left');
+      console.log('left')
       break;
     case 39: //right
-      console.log('right');
+      console.log('right')
       seperateMovementFunction('col', '+')
       break;
   }
@@ -30,23 +32,23 @@ Game.prototype.moveTile = function(tile, direction) {
       }
 
 
-      this.dataset.col = type + num.toString()
+      this.dataset[type] = type + num.toString()
     })
   }
 
-};
+}
 
-$(document).ready(function() {
-  console.log("ready to go!");
+$(document).ready(function () {
+  console.log("ready to go!")
   // Any interactive jQuery functionality
   var game = new Game();
 
-  $('body').keydown(function(event){
+  $('body').keydown(function(event) {
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
-      var tile = $('.tile');
+      var tile = $('.tile')
 
-      game.moveTile(tile, event.which);
+      game.moveTile(tile, event.which)
     }
-  });
-});
+  })
+})
