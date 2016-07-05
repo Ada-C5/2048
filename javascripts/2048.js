8189ba2cf61630f128078ceff9183fb6b11d28af
Ania Gonzalez <aniagr@gmail.com>
2016-07-05 16:09:24 -0700

    We created a function that renders the tiles

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,22 +2,22 @@ var Game = function() {
   // Game logic and initialization here
   //add rando first value
   this.matrix = [
-    {col:0, row: 0, val:0 },
-    {col:1, row: 0, val:0 },
-    {col:2, row: 0, val:0 },
-    {col:3, row: 0, val:0 },
-    {col:0, row: 1, val:0 },
-    {col:1, row: 1, val:0 },
-    {col:2, row: 1, val:0 },
-    {col:3, row: 1, val:0 },
-    {col:0, row: 2, val:0 },
-    {col:1, row: 2, val:0 },
-    {col:2, row: 2, val:0 },
-    {col:3, row: 2, val:0 },
-    {col:0, row: 3, val:0 },
-    {col:1, row: 3, val:0 },
-    {col:2, row: 3, val:0 },
-    {col:3, row: 3, val:0 }
+    {col:"c0", row: "r0", val:"" },
+    {col:"c1", row: "r0", val:"" },
+    {col:"c2", row: "r0", val:"" },
+    {col:"c3", row: "r0", val:"" },
+    {col:"c0", row: "r1", val:"" },
+    {col:"c1", row: "r1", val:"" },
+    {col:"c2", row: "r1", val:"" },
+    {col:"c3", row: "r1", val:"" },
+    {col:"c0", row: "r2", val:"" },
+    {col:"c1", row: "r2", val:"" },
+    {col:"c2", row: "r2", val:"" },
+    {col:"c3", row: "r2", val:"" },
+    {col:"c0", row: "r3", val:"" },
+    {col:"c1", row: "r3", val:"" },
+    {col:"c2", row: "r3", val:"" },
+    {col:"c3", row: "r3", val:"" }
   ]
   var initialSquare = this.matrix[Math.floor(Math.random()*this.matrix.length)];
   console.log(initialSquare)
@@ -44,7 +44,6 @@ Game.prototype.moveTile = function(tile, direction) {
       break;
   }
 
-
 };
 
 
@@ -62,4 +61,21 @@ $(document).ready(function() {
       game.moveTile(tile, event.which);
     }
   });
+
+
+  // <div class="tile" data-row="r2", data-col="c2" data-val="4">4</div>
+
+  var tilex = $(".tile") //al 16 of them
+
+  for (var i=0, max=tilex.length; i < max; i++) {
+     // Do something with the element here
+     console.log(tilex[i])
+     console.log(game.matrix[i])
+     tilex[i].dataset.row = game.matrix[i]['row']
+     tilex[i].dataset.col = game.matrix[i]['col']
+     tilex[i].dataset.val = game.matrix[i]['val']
+     tilex[i].innerHTML = game.matrix[i]['val']
+  }
+
+
 });
