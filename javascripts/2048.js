33b0edf830b434496a344e776edefec57a762365
Adriana <dri19tcc@gmail.com>
2016-07-05 16:42:14 -0700

    Added functionality so that tiles slide each time an arrow is pushed

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -7,9 +7,11 @@ Game.prototype.moveTile = function(tiles, direction) {
   switch(direction) {
     case 38: //up
       console.log('up');
+      this.moveUp()
       break;
     case 40: //down
       console.log('down');
+      this.moveDown()
       break;
     case 37: //left
       console.log('left');
@@ -17,13 +19,29 @@ Game.prototype.moveTile = function(tiles, direction) {
       break;
     case 39: //right
       console.log('right');
+      this.moveRight()
       break;
   }
 };
 
 Game.prototype.moveLeft = function(){
   var tiles = $(".tile")
-  console.log(tiles.data("row"))
+  tiles.attr('data-col', 'c0')
+};
+
+Game.prototype.moveUp = function(){
+  var tiles = $(".tile")
+  tiles.attr('data-row', 'r0')
+};
+
+Game.prototype.moveRight = function(){
+  var tiles = $(".tile")
+  tiles.attr('data-col', 'c3')
+};
+
+Game.prototype.moveDown = function(){
+  var tiles = $(".tile")
+  tiles.attr('data-row', 'r3')
 };
 
 $(document).ready(function() {
@@ -35,7 +53,7 @@ $(document).ready(function() {
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
       var tiles = $('.tile');
-      // console.log(event.which)
+      // console.log(tiles)
       game.moveTile(tiles, event.which);
     }
   });
