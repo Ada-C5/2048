42a6c660e2c1821ba3ae3f237f0c982c63e26d0f
Ania Gonzalez <aniagr@gmail.com>
2016-07-05 16:25:52 -0700

    Refactored columns and rows into numbers so we can use them in ranges

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,29 +2,29 @@ var Game = function() {
   // Game logic and initialization here
   //add rando first value
   this.matrix = [
-    {col:"c0", row: "r0", val:"" },
-    {col:"c1", row: "r0", val:"" },
-    {col:"c2", row: "r0", val:"" },
-    {col:"c3", row: "r0", val:"" },
-    {col:"c0", row: "r1", val:"" },
-    {col:"c1", row: "r1", val:"" },
-    {col:"c2", row: "r1", val:"" },
-    {col:"c3", row: "r1", val:"" },
-    {col:"c0", row: "r2", val:"" },
-    {col:"c1", row: "r2", val:"" },
-    {col:"c2", row: "r2", val:"" },
-    {col:"c3", row: "r2", val:"" },
-    {col:"c0", row: "r3", val:"" },
-    {col:"c1", row: "r3", val:"" },
-    {col:"c2", row: "r3", val:"" },
-    {col:"c3", row: "r3", val:"" }
+    {col:0, row:0, val:"" },
+    {col:1, row:0, val:"" },
+    {col:2, row:0, val:"" },
+    {col:3, row:0, val:"" },
+    {col:0, row:1, val:"" },
+    {col:1, row:1, val:"" },
+    {col:2, row:1, val:"" },
+    {col:3, row:1, val:"" },
+    {col:0, row:2, val:"" },
+    {col:1, row:2, val:"" },
+    {col:2, row:2, val:"" },
+    {col:3, row:2, val:"" },
+    {col:0, row:3, val:"" },
+    {col:1, row:3, val:"" },
+    {col:2, row:3, val:"" },
+    {col:3, row:3, val:"" }
   ]
   var initialSquare = this.matrix[Math.floor(Math.random()*this.matrix.length)];
-  console.log(initialSquare)
+  // console.log(initialSquare)
 
   const initialValues = [2, 4]
   initialSquare['val'] = initialValues[Math.floor(Math.random()*initialValues.length)];
-  console.log(initialSquare)
+  // console.log(initialSquare)
 };
 
 Game.prototype.moveTile = function(tile, direction) {
@@ -32,15 +32,28 @@ Game.prototype.moveTile = function(tile, direction) {
   switch(direction) {
     case 38: //up
       console.log('up');
+      //check each square above the designated square ranging R0-R3
+      // if tile_hash[row] === R0
+        // check all squares with the same column value with row values 1-3 to see if they are occupied
+      // else if tile_hash[row] === R1
+        // check all squares with the same column value with row values 2-3 to see if they are occupied
+      // else if tile_hash[row] === R2
+        // check square with same column value and R3 to see if it is occupied
+
+
       break;
     case 40: //down
       console.log('down');
+      //check each square below the designated square ranging R3-R0
       break;
     case 37: //left
       console.log('left');
+      //check each square right of the designated square ranging C0-C3
+
       break;
     case 39: //right
       console.log('right');
+      //check each square left of the designated square ranging C3-C0
       break;
   }
 
@@ -69,10 +82,10 @@ $(document).ready(function() {
 
   for (var i=0, max=tilex.length; i < max; i++) {
      // Do something with the element here
-     console.log(tilex[i])
-     console.log(game.matrix[i])
-     tilex[i].dataset.row = game.matrix[i]['row']
-     tilex[i].dataset.col = game.matrix[i]['col']
+    //  console.log(tilex[i])
+    //  console.log(game.matrix[i])
+     tilex[i].dataset.row = "r" + (game.matrix[i]['row']).toString()
+     tilex[i].dataset.col = "c" + (game.matrix[i]['col']).toString()
      tilex[i].dataset.val = game.matrix[i]['val']
      tilex[i].innerHTML = game.matrix[i]['val']
   }
