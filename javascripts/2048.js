a5982cdf228add02e19bed222bbda8ba844ee051
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-05 13:36:40 -0700

    just a test

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,5 +1,6 @@
 var Game = function() {
   // Game logic and initialization here
+  //let's do this!
 };
 
 Game.prototype.moveTile = function(tile, direction) {
@@ -29,7 +30,7 @@ $(document).ready(function() {
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
       var tile = $('.tile');
-      
+
       game.moveTile(tile, event.which);
     }
   });
