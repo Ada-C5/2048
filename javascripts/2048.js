9e0d24e7b5e0d7596a84bdb1173f2fdc80e9c9d5
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-07-05 16:18:27 -0700

    Added board object and function to update html

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,5 +1,8 @@
 var Game = function() {
-  // Game logic and initialization here
+  this._board = [[null, null, null, null],
+                [null, null, null, null],
+                [null, null, null, null],
+                [null, null, null, null]]
 };
 
 Game.prototype.moveTile = function(tile, direction) {
@@ -20,6 +23,15 @@ Game.prototype.moveTile = function(tile, direction) {
   }
 };
 
+Game.prototype.updateBoard = function() {
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      slot = "[" + i.toString() + "][" + j.toString() + "]"
+      $(slot).contents() = this._board[i][j]
+    }
+  }
+}
+
 $(document).ready(function() {
   console.log("ready to go!");
   // Any interactive jQuery functionality
@@ -29,7 +41,7 @@ $(document).ready(function() {
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
       var tile = $('.tile');
-      
+
       game.moveTile(tile, event.which);
     }
   });
