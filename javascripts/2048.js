4d62ccf45a6997537f1d78acefef06b2093c43f1
Cristal Tay <bluecolorpencils@gmail.com>
2016-07-06 13:59:52 -0700

    holy crap it moves and adds

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,21 +1,84 @@
 var Game = function() {
   // Game logic and initialization here
   this._board = [
-    [2,0,0,0],
-    [2,0,0,0],
-    [4,0,0,0],
-    [4,0,0,0]
+    [2,2,0,2],
+    [2,0,2,2],
+    [0,2,0,0],
+    [2,0,0,4]
   ]
 };
+
+Game.prototype.moveAll = function(tile) {
+
+
+  // create matrix each row represents a column
+  var matrix = []
+  var matrixValues = []
+  for (let i = 0; i < tile.length; i++){
+    var queryStr = '.tile[data-col=' + i +']'
+    let row = $(queryStr).sort(function (a, b) {
+      return +a.getAttribute('data-row') - +b.getAttribute('data-row');
+    })
+    matrix.push(row)
+    matrixValues.push(row.text().split(""))
+  }
+//   [
+//   [2, 4, 2],
+//   [4, 2, 2],
+//   [2, 2],
+//   [4]
+// ]
+  //
+  // var column = []
+  for (let j =0; j < matrix.length -1 ; j++){ //j is column
+    // for (let i=0; i<4; i++) { // i is the row
+    //   if (matrix[i][j] !== 0 ){
+    //     column.push(matrix[i][0]);
+    //   }
+    // }
+    // console.log(matrix[0][0])
+    for (let i=0; i< matrixValues[j].length; i++) {
+      if (matrixValues[j][i] == matrixValues[j][i+1]) {
+
+        // CHANGES VAL AND DATA
+        var value = matrixValues[j][i]*2;
+        matrixValues[j][i] = value
+        $(matrix[j][i]).attr({ "data-val": value.toString() });
+        $(matrix[j][i]).text(value);
+
+        // DELETES
+        $(matrixValues[j][i]).attr({ "data-row": "0" });
+        // matrix[j].splice(i+1,1)
+        console.log("HI: ", matrix[i][j])
+        $(matrix[j][i+1]).remove();
+
+        // CHANGE INDEXES
+
+           //
+          //  change matrix[0].attr({ "data-val": "4" })
+          //     matrix[0].text(4)
+           //
+          //     matrix[0].remove()
+          //
+        // matrix[]
+        // .delete the div
+      }
+    }
+    // console.log(column)
+  }
+
+  console.log(matrix)
+}
 
 Game.prototype.moveTile = function(tile, direction) {
   // Game method here
   switch(direction) {
     case 38: //up
 
-      console.log('up');
+      console.log(tile);
       // for (let i = 0; i < tile.length; i++)
-      tile.attr({ "data-row": "0" });
+      // tile.attr({ "data-row": "0" });
+      this.moveAll(tile)
       break;
 
     case 40: //down
@@ -32,112 +95,7 @@ Game.prototype.moveTile = function(tile, direction) {
   }
 };
 
-Game.prototype.moveAll = function() {
-  var column = []
-  for (let i=0; i<4; i++) {
-    if (this._board[i][0] !== 0 ){
-      column.push(this._board[i][0]);
-    }
-  }
-  console.log(column)
 
-  for (let i=0; i<column.length; i++) {
-    if (column[i] == column[i+1]) {
-      column[i] = column[i]*2;
-      column.splice(i+1,1)
-      console.log(column)
-    }
-  }
-  // console.log("START", this._board)
-  // for (let col = 0; col < 4; col++) {
-  //   let pointer = 0;
-  //   let queryStr = "";
-  //   let tempAdd = 0;
-  //   let tempAddRow = 0;
-  //   let currentValue = 0;
-  //
-  //   for (let row = 0; row < 4; row++) {
-  //     // '.tile[data-row=row]'
-  //
-  //     currentValue = this._board[row][col]
-  //     // console.log("")
-  //     console.log("CV: "+ currentValue + " Row: " + row + " TempAdd: " + tempAdd + " TempRow: " + tempAddRow)
-  //     if (currentValue == 0 && row != 3) {
-  //       // tempAdd
-  //       console.log("skip")
-  //       continue;
-  //     }
-  //
-  //     if ((currentValue > 0 ) && (tempAdd != 0) && (currentValue == tempAdd)) {
-  //       console.log("FIRST")
-  //
-  //       this._board[pointer][col] = currentValue*2 ;
-  //       this._board[row][col] = 0;
-  //       pointer++;
-  //       tempAdd = 0;
-  //       // tempAddRow = row;
-  //       // jquery to delete currentValue div
-  //     } else if (tempAdd != 0 && tempAddRow != 0) {
-  //       if (row == 3) {
-  //         console.log("SECOND")
-  //         this._board[pointer][col] = tempAdd
-  //         this._board[tempAddRow][col] = 0;
-  //         pointer++
-  //         this._board[pointer][col] = currentValue;
-  //         this._board[row][col] = 0;
-  //         // currentValue
-  //         break;
-  //       } else {
-  //         console.log("THIRD")
-  //         this._board[pointer][col] = tempAdd;
-  //         this._board[tempAddRow][col] = 0;
-  //         this._board[row][col] = 0;
-  //         tempAdd = 0;
-  //         // tempAddRow = row;
-  //         pointer ++
-  //       }
-  //     }
-  //
-  //     if (row === 3 && tempAdd === 0) {
-  //       console.log("FIFTH")
-  //       this._board[pointer][col] = currentValue;
-  //       this._board[row][col] = 0;
-  //     }
-  //
-  //     if ((tempAdd != 0) && (currentValue != tempAdd)) {
-  //       // tempAdd != 0 && tempAddRow === 0) {
-  //       console.log("FIF SIX")
-  //       this._board[pointer][col] = tempAdd;
-  //       pointer++
-  //       if (row === 3) {
-  //       this._board[pointer][col] = tempAdd;
-  //     }
-  //       // this._board[]
-  //
-  //     }
-  //     // } else if (row === 3 && tempAdd != 0)
-  //
-  //     if (currentValue > 0) {
-  //       console.log("SIXTH")
-  //       tempAdd = currentValue;
-  //       tempAddRow = row;
-  //       // console.log("TEMPADD: " + tempAdd + " tEMPROW: " + tempAddRow)
-  //       // console.log(this._board)
-  //     }
-  //
-  //
-  //   }
-  //   // console.log(col)
-  // }
-  // return this._board
-}
-
-
-
-      // .dataset
-      // [0].dataset.row
-      // if
-      // pointer++
       // $('.tile[data-row=0]')
 
 $(document).ready(function() {
@@ -146,7 +104,7 @@ $(document).ready(function() {
   var game = new Game();
     console.log("START000000:", game._board)
 
-  console.log(game.moveAll())
+  // console.log(game.moveAll())
 
   $('body').keydown(function(event){
     var arrows = [37, 38, 39, 40];
