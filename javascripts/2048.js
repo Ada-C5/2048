299080ebf1b4dde960616d2824a46f9b091c7ddf
Cristal Tay <bluecolorpencils@gmail.com>
2016-07-06 13:07:20 -0700

    made arrays work. going to combine yo's div logic

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,10 +1,10 @@
 var Game = function() {
   // Game logic and initialization here
   this._board = [
+    [2,0,0,0],
+    [2,0,0,0],
     [4,0,0,0],
-    [2,0,0,0],
-    [2,0,0,0],
-    [2,0,0,0]
+    [4,0,0,0]
   ]
 };
 
