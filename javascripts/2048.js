f3d799b381847b602f45dc599a874ed53ea3a16b
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-06 12:28:36 -0700

    about to extract new tile method "

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,21 +1,21 @@
 var Game = function() {
   this.board = {
-    "row0col0": null,
-    "row1col0": null,
-    "row2col0": null,
-    "row3col0": null,
-    "row0col1": null,
-    "row1col1": null,
-    "row2col1": null,
-    "row3col1": null,
-    "row0col2": null,
-    "row1col2": null,
-    "row2col2": null,
-    "row3col2": null,
-    "row0col3": null,
-    "row1col3": null,
-    "row2col3": null,
-    "row3col3": null
+    "r0c0": null,
+    "r1c0": null,
+    "r2c0": null,
+    "r3c0": null,
+    "r0c1": null,
+    "r1c1": null,
+    "r2c1": null,
+    "r3c1": null,
+    "r0c2": null,
+    "r1c2": null,
+    "r2c2": null,
+    "r3c2": null,
+    "r0c3": null,
+    "r1c3": null,
+    "r2c3": null,
+    "r3c3": null
   }
 };
 
@@ -23,6 +23,11 @@ Game.prototype.moveTile = function(tile, direction) {
   // Game method here
   switch(direction) {
     case 38: //up
+    //check tile directly above to see if its empty
+    //if that's empty, check next one
+    // do that again if not at top yet
+    //once next tile is not empty, check its value
+    // if that value is the same, move up and collide, run score tile method
     tile.attr({
       "data-row": "r0",
       "data-val": '8',
@@ -52,13 +57,25 @@ Game.prototype.moveTile = function(tile, direction) {
 };
 
 Game.prototype.newTile = function() {
-  var rows = ["r0", "r1", "r2", 'r3']
-  var columns = ['c0', 'c1', 'c2', 'c3']
-  var row = rows[Math.floor(Math.random() * rows.length)]
-  var column = columns[Math.floor(Math.random() * columns.length)]
-  // checks randomized tile locale
-  var checkSpace = $('#gameboard').find("[data-col='" + column + "'][data-row='" + row + "']").length
-  console.log(column, row, checkSpace)
+  // var rows = ["r0", "r1", "r2", 'r3']
+  // var columns = ['c0', 'c1', 'c2', 'c3']
+  // var row = rows[Math.floor(Math.random() * rows.length)]
+  // var column = columns[Math.floor(Math.random() * columns.length)]
+  // var space = row + column
+  // while (this.board[space] != null) {
+  //   var rows = ["r0", "r1", "r2", 'r3']
+  //   var columns = ['c0', 'c1', 'c2', 'c3']
+  //   var row = rows[Math.floor(Math.random() * rows.length)]
+  //   var column = columns[Math.floor(Math.random() * columns.length)]
+  //   var space = row + column
+  //   console.log('hi')
+  // }
+  // var checkSpace = $('#gameboard').find("[data-col='" + column + "'][data-row='" + row + "']").length
+
+  
+
+
+  // console.log(checkSpace)
 
   var rand = ['2','4']
   var newTileValue = rand[Math.floor(Math.random() * rand.length)]
@@ -66,6 +83,11 @@ Game.prototype.newTile = function() {
 
   var damn = "<div class='tile' data-row=" + row + " data-col=" + column + " data-val=" + newTileValue + ">" + newTileValue + "</div>"
   var newTile = $("#gameboard").append(damn);
+  var property = row + column
+  this.board[property] = newTileValue
+  console.log(this.board)
+}
+
 
 $(document).ready(function() {
   console.log("ready to go!");
