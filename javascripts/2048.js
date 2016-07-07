ab1a46e3e2b52315d59b40b7b239e4e13e5967dc
Valerie <val.conklin@gmail.com>
2016-07-07 11:30:25 -0700

    Tiles can now properly move up and down without overlapping, and that functionality is isolated in its own prototypal method

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -23,41 +23,69 @@ Game.prototype.newTile = function() {
   // $('#gameboard').append(tile_div)
 }
 
+Game.prototype.sortAndMoveUp = function(tiles) {
+  tiles = tiles.sort(function (a, b) {
+    var aRowNumber = parseInt($(a).attr('data-row').substring(1))
+    var bRowNumber = parseInt($(b).attr('data-row').substring(1))
+    return aRowNumber - bRowNumber
+  })
+
+  for (var i = 0; i < tiles.length; i++) {
+    $(tiles[i]).attr('data-row', 'r' + i)
+  }
+}
+
+Game.prototype.sortAndMoveDown = function(tiles) {
+  tiles = tiles.sort(function (a, b) {
+    var aRowNumber = parseInt($(a).attr('data-row').substring(1))
+    var bRowNumber = parseInt($(b).attr('data-row').substring(1))
+    return bRowNumber - aRowNumber
+  })
+
+  for (var i = 0; i < tiles.length; i++) {
+    $(tiles[i]).attr('data-row', 'r' + (3 - i))
+  }
+}
+
 Game.prototype.moveTile = function(tiles, direction) {
   // Game method here
+  //filters from all the tiles
+  var tilesc0 = tiles.filter('[data-col=c0]').toArray()
+  var tilesc1 = tiles.filter('[data-col=c1]').toArray()
+  var tilesc2 = tiles.filter('[data-col=c2]').toArray()
+  var tilesc3 = tiles.filter('[data-col=c3]').toArray()
+
   switch(direction) {
     case 38: //up
       console.log('up');
 
-      //filters from all the tiles
-      var tilesc0 = tiles.filter('[data-col=c0]').toArray()
-
-      //the anonymous function in sort compares between 2 elements (tiles in this case) in the array
-      tilesc0 = tilesc0.sort(function (a, b) {
-        var aRowNumber = parseInt($(a).attr('data-row').substring(1))
-        var bRowNumber = parseInt($(b).attr('data-row').substring(1))
-        return aRowNumber - bRowNumber //if a is a higher number, then it will be positive. sort function assumes that if it's a positive number, then it knows the 'a' number is the higher one.
-        // if I was trying to move downward, then I need to do the opposite and return bRowNumber - aRowNumber, which would give me a reversed order (the highest number would be in the first index)
-      })
-
-      //because we need the index
-      for (var i = 0; i < tilesc0.length; i++) {
-        $(tilesc0[i]).attr('data-row', 'r' + i)
-      }
-
+      // will DRY into separate method like this later...
+      this.sortAndMoveUp(tilesc0)
+      this.sortAndMoveUp(tilesc1)
+      this.sortAndMoveUp(tilesc2)
+      this.sortAndMoveUp(tilesc3)
 
       break;
     case 40: //down
       console.log('down');
-      $('.tile').attr("data-row", "r3");
+
+      this.sortAndMoveDown(tilesc0)
+      this.sortAndMoveDown(tilesc1)
+      this.sortAndMoveDown(tilesc2)
+      this.sortAndMoveDown(tilesc3)
+
       break;
     case 37: //left
       console.log('left');
-      $('.tile').attr("data-col", "c0");
+
+
+
       break;
     case 39: //right
       console.log('right');
-      $('.tile').attr("data-col", "c3");
+
+
+
       break;
   }
 };
