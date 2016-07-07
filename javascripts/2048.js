575d8f72461acdbcc229449ebdc2ef83ae575219
Lisa Rolczynski <lisa.sano@gmail.com>
2016-07-07 16:18:26 -0700

    fixed bug that was adding new tiles even if the existing tiles couldn't move or combine at all in the given direction

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -25,11 +25,9 @@ var Game = function() {
 
   function axis_loss(axis) {
     for (let n = 0; n < 4; n++) {
-      array = $(".tile[data-" + axis + "=" + axis[0] + n + "]");
+      let array = $(".tile[data-" + axis + "=" + axis[0] + n + "]");
       for (let i = 0; i < array.length; i++) {
         if (array[i+1] && $(array)[i].dataset.val === $(array)[i+1].dataset.val) {
-          console.log(array[i]);
-          console.log(array[i+1]);
           return false;
         }
       }
@@ -39,22 +37,24 @@ var Game = function() {
 
 Game.prototype.moveTile = function(tile, direction) {
   // Game method here
-  var available,
+  let available,
   axis_index,
   axis,
-  array;
+  array,
+  moved = false;
 
   switch(direction) {
     case 38: //up
-    console.log('up');
     available = [];
     axis = "row";
     for (let n = 0; n < 4; n++) {
       axis_index = 0;
-      var array = $(".tile[data-col=c" + n + "]");
+      array = $(".tile[data-col=c" + n + "]");
       sort_things(array, "data-row", "up");
       for (let i = 0; i < 4; i++) {
-        slide_tile(array, axis_index, i, axis);
+        if (slide_tile(array, axis_index, i, axis)) {
+          moved = true;
+        }
         axis_index++;
       }
       // 4 - tiles.count - how many spaces available
@@ -66,19 +66,25 @@ Game.prototype.moveTile = function(tile, direction) {
         available.push(['c' + n, 'r' + col]);
       }
     }
-    return available;
+
+    if (moved === false) {
+      return moved;
+    } else {
+      return available;
+    }
 
     case 40: //down
-    console.log('down');
     available = [];
     axis = "row";
     // tile.attr({ "data-row": "r2" });
     for (let n = 0; n < 4; n++) {
       axis_index = 3;
-      var array = $(".tile[data-col=c" + n + "]");
+      array = $(".tile[data-col=c" + n + "]");
       sort_things(array, "data-row", "down");
       for (let i = 0; i < 4; i++) {
-        slide_tile(array, axis_index, i, axis);
+        if (slide_tile(array, axis_index, i, axis)) {
+          moved = true;
+        }
         axis_index--;
       }
       // 4 - tiles.count - how many spaces available
@@ -89,10 +95,14 @@ Game.prototype.moveTile = function(tile, direction) {
         available.push(['c' + n, 'r' + i]);
       }
     }
-    return available;
+
+    if (moved === false) {
+      return moved;
+    } else {
+      return available;
+    }
 
     case 37: //left
-    console.log('left');
     available = [];
     axis = "col";
     // axis = "data-col";
@@ -102,7 +112,9 @@ Game.prototype.moveTile = function(tile, direction) {
       array = $(".tile[data-row=r" + n + "]");
       sort_things(array, "data-col", "left");
       for (let i = 0; i < array.length; i++) {
-        slide_tile(array, axis_index, i, axis)
+        if (slide_tile(array, axis_index, i, axis)) {
+          moved = true;
+        }
         axis_index++
       }
       // 4 - tiles.count - how many spaces available
@@ -114,10 +126,14 @@ Game.prototype.moveTile = function(tile, direction) {
         available.push(['c' + col, 'r' + n]);
       }
     }
-    return available;
+
+    if (moved === false) {
+      return moved;
+    } else {
+      return available;
+    }
 
     case 39: //right
-    console.log('right');
     available = [];
     axis = "col";
     for (let n = 0; n < 4; n++) {
@@ -125,8 +141,9 @@ Game.prototype.moveTile = function(tile, direction) {
       array = $(".tile[data-row=r" + n + "]");
       sort_things(array, "data-col", "right");
       for (let i = 0; i < array.length; i++) {
-        slide_tile(array, axis_index, i, axis);
-        console.log(axis_index);
+        if (slide_tile(array, axis_index, i, axis)) {
+          moved = true;
+        }
         axis_index--;
       }
       // 4 - tiles.count - how many spaces available
@@ -137,7 +154,12 @@ Game.prototype.moveTile = function(tile, direction) {
         available.push(['c' + i, 'r' + n]);
       }
     }
-    return available;
+
+    if (moved === false) {
+      return moved;
+    } else {
+      return available;
+    }
   }
 };
 
@@ -164,13 +186,21 @@ function sort_things(tile_array, sort_by, direction) {
 }
 
 function slide_tile(array, axis_index, i, axis) {
+  let altered = false;
   if (axis === "col") {
+    if (array[i] && $(array[i]).attr("data-col") !== "c" + axis_index) {
+      altered = true;
+    }
     $(array[i]).attr({ "data-col": "c" + axis_index });
   } else {
+    if (array[i] && $(array[i]).attr("data-row") !== "r" + axis_index) {
+      altered = true;
+    }
     $(array[i]).attr({ "data-row": "r" + axis_index });
   }
   // if two values next to each other are the same
   if (array[i+1] && $(array)[i].dataset.val === $(array)[i+1].dataset.val) {
+    altered = true;
     // move the 2nd value to where the 1st one is
     if (axis === "col") {
       $(array[i+1]).attr({ "data-col": "c" + axis_index });
@@ -191,6 +221,10 @@ function slide_tile(array, axis_index, i, axis) {
     }, 200);
     // remove from the array
     array.splice(i+1, 1);
+  }
+
+  if (altered) {
+    return altered
   }
 }
 
@@ -216,9 +250,9 @@ Game.prototype.new_game = function() {
 
   for (let i = 0; i < 2; i++) {
     let r_num = Math.floor(Math.random() * (3 + 1) + 0);
-    console.log(r_num)
+    // console.log(r_num)
     let c_num = Math.floor(Math.random() * (3 + 1) + 0);
-    console.log(c_num)
+    // console.log(c_num)
     let vals = [2, 2, 2, 2, 2, 2, 4]
     var val = vals[Math.floor(Math.random() * vals.length)];
 
