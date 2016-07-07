fb118f0861f65a79436824ff3fb6457fe92d3a42
Valerie <val.conklin@gmail.com>
2016-07-07 14:26:10 -0700

    Tiles can move left and right now

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -47,6 +47,30 @@ Game.prototype.sortAndMoveDown = function(tiles) {
   }
 }
 
+Game.prototype.sortAndMoveLeft = function(tiles) {
+  tiles = tiles.sort(function (a, b) {
+    var aColNumber = parseInt($(a).attr('data-col').substring(1))
+    var bColNumber = parseInt($(b).attr('data-col').substring(1))
+    return aColNumber - bColNumber
+  })
+
+  for (var i = 0; i < tiles.length; i++) {
+    $(tiles[i]).attr('data-col', 'c' + i)
+  }
+}
+
+Game.prototype.sortAndMoveRight = function(tiles) {
+  tiles = tiles.sort(function (a, b) {
+    var aColNumber = parseInt($(a).attr('data-col').substring(1))
+    var bColNumber = parseInt($(b).attr('data-col').substring(1))
+    return bColNumber - aColNumber
+  })
+
+  for (var i = 0; i < tiles.length; i++) {
+    $(tiles[i]).attr('data-col', 'c' + (3 - i))
+  }
+}
+
 Game.prototype.moveTile = function(tiles, direction) {
   // Game method here
   //filters from all the tiles
@@ -54,6 +78,11 @@ Game.prototype.moveTile = function(tiles, direction) {
   var tilesc1 = tiles.filter('[data-col=c1]').toArray()
   var tilesc2 = tiles.filter('[data-col=c2]').toArray()
   var tilesc3 = tiles.filter('[data-col=c3]').toArray()
+
+  var tilesr0 = tiles.filter('[data-row=r0]').toArray()
+  var tilesr1 = tiles.filter('[data-row=r1]').toArray()
+  var tilesr2 = tiles.filter('[data-row=r2]').toArray()
+  var tilesr3 = tiles.filter('[data-row=r3]').toArray()
 
   switch(direction) {
     case 38: //up
@@ -78,13 +107,19 @@ Game.prototype.moveTile = function(tiles, direction) {
     case 37: //left
       console.log('left');
 
-
+      this.sortAndMoveLeft(tilesr0)
+      this.sortAndMoveLeft(tilesr1)
+      this.sortAndMoveLeft(tilesr2)
+      this.sortAndMoveLeft(tilesr3)
 
       break;
     case 39: //right
       console.log('right');
 
-
+      this.sortAndMoveRight(tilesr0)
+      this.sortAndMoveRight(tilesr1)
+      this.sortAndMoveRight(tilesr2)
+      this.sortAndMoveRight(tilesr3)
 
       break;
   }
