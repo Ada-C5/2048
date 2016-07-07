e3f587407401fc2a6bb3668b13a4504a47beabdf
melissajimison <melissajimison@gmail.com>
2016-07-07 10:24:51 -0700

    Created helper methods to move all the way to one direction and to merge if same value. Alot of oportunities to refactor though

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -23,6 +23,7 @@ Game.prototype.moveTile = function (tile, direction) {
   }
 
   function seperateMovementFunction(type, operand) {
+
     // tile.each(function() {
     //   let num = parseInt(this.dataset[type][3])
     //   if (operand === '+') {
@@ -32,53 +33,86 @@ Game.prototype.moveTile = function (tile, direction) {
     //   }
     // })
     // this.dataset[type] = type + num.toString()
+    //gets all the tiles in the second row
+      // arrayTiles = $('.tile[data-row=row1]')
 
-      //gets all the tiles in the second row
-      arrayTiles = $('.tile[data-row=row1]')
-      // console.log(event);
-    $.each(arrayTiles, function( index, value ) {
-      //convert the colunm into an integer
-      let num = parseInt(value.dataset["col"][3])
-      //defines the next colunm
-      let nextEle = num + 1
-      // console.log(num);
-      while (nextEle < 4) {
+      // console.log(arrayTiles[2]);
+      arrayTiles = $('.tile')
 
-        let nextString = nextEle.toString()
-        //interpolates the next element
-        let nextCol = '.tile[data-col=' + type + nextString + ']'
-        let nextColEle = $(nextCol)
+    moveAllTheWay(arrayTiles, type)
 
-        //check if next tile is empty,
-          if (nextColEle.length == 0) {
-            value.dataset[type] = type + nextString
-          }
+    collideIfSameValue(arrayTiles,type)
 
-          if (nextColEle.length > 0) {
+    moveAllTheWay(arrayTiles,  type)
 
-            if (value.dataset.val === nextColEle[0].dataset.val) {
-              console.log("melissa");
-              value.dataset[type] = type + nextString
-              value.dataset.val = parseInt(value.dataset.val) + parseInt(nextColEle[0].dataset.val)
-              setTimeout(function(){
-                $(value).text(value.dataset.val)
-              }, 300);
-            }
-          }
-
-          //keep moving next while empty, and colum is equal or less than 3
-      nextEle++;
-      }
-
-          //if it is not empty
-            //if it is the same value, sum and keep moving
-              //elseif is different, grab that one and star looking for next empty tile
-
-    });
 
   }
 
+  function moveAllTheWay(arrayQuerys, type) {
+    $.each(arrayQuerys, function( index, value ) {
+      //convert the colunm into an integer
+      let numRow = value.dataset["row"][3]
+      let num = parseInt(value.dataset["col"][3])
+      //defines the next colunm
+      var nextEle = num + 1
+      var nextString = nextEle.toString()
+      //interpolates the next element
+      var nextCol = '.tile[data-row=row'+ numRow + '][data-col=' + type + nextString + ']'
+      var nextColEle = $(nextCol)
+
+      //if it is not empty
+
+      while (nextEle < 4) {
+        let nextString = nextEle.toString()
+        //interpolates the next element
+        let nextCol = '.tile[data-row=row'+ numRow + '][data-col=' + type + nextString + ']'
+        let nextColEle = $(nextCol)
+        // console.log(nextColEle);
+
+        //check if next tile is empty,
+        if (nextColEle.length == 0) {
+          value.dataset[type] = type + nextString
+        }
+
+
+        //keep moving next while empty, and colum is equal or less than 3
+        nextEle++;
+      }
+    });
+  }
+
+  function collideIfSameValue(arrayTiles,type) {
+    $.each(arrayTiles, function( index, value ) {
+
+      let numRow = value.dataset["row"][3]
+      let num = parseInt(value.dataset["col"][3])
+      //defines the next colunm
+      let nextEle = num + 1
+      let nextString = nextEle.toString()
+      //interpolates the next element
+      let nextCol = '.tile[data-row=row'+ numRow + '][data-col=' + type + nextString + ']'
+      let nextColEle = $(nextCol)
+
+          //if it is the same value, sum and keep moving
+            //elseif is different, grab that one and star looking for next empty tile
+            if (nextColEle.length > 0) {
+              //chek if value is the same than current tile
+              if (value.dataset.val === nextColEle[0].dataset.val) {
+                value.dataset[type] = type + nextString
+                value.dataset.val = parseInt(value.dataset.val) + parseInt(nextColEle[0].dataset.val)
+                setTimeout(function(){
+                  $(value).text(value.dataset.val)
+                }, 240);
+              }
+            }
+    });
+  }
+
 }
+
+
+
+
 
 $(document).ready(function () {
   console.log("ready to go!")
