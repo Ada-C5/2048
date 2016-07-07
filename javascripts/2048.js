a947d48a54106f87aad16724d1f44b7a23ef20c2
justine <justine.winnie@gmail.com>
2016-07-07 15:18:18 -0700

    new game button working with new tiles appearing.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -20,112 +20,112 @@ var Game = function() {
 Game.prototype.moveTile = function(tile, direction) {
   // Game method here
   var available,
-      axis_index,
-      axis,
-      array;
+  axis_index,
+  axis,
+  array;
 
   switch(direction) {
     case 38: //up
-      console.log('up');
-      available = [];
-      axis = "row";
-      for (let n = 0; n < 4; n++) {
-        axis_index = 0;
-        var array = $(".tile[data-col=c" + n + "]");
-        sort_things(array, "data-row", "up");
-        for (let i = 0; i < 4; i++) {
-          slide_tile(array, axis_index, i, axis);
-          axis_index++;
-        }
-        // 4 - tiles.count - how many spaces available
-        let available_count = 4 - array.length;
-        // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
-        for (let i = 0; i < available_count; i++) {
-          let col = 3 - i;
-          // when looping through all rows, change the row value
-          available.push(['c' + n, 'r' + col]);
-        }
+    console.log('up');
+    available = [];
+    axis = "row";
+    for (let n = 0; n < 4; n++) {
+      axis_index = 0;
+      var array = $(".tile[data-col=c" + n + "]");
+      sort_things(array, "data-row", "up");
+      for (let i = 0; i < 4; i++) {
+        slide_tile(array, axis_index, i, axis);
+        axis_index++;
       }
-      return available;
+      // 4 - tiles.count - how many spaces available
+      let available_count = 4 - array.length;
+      // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
+      for (let i = 0; i < available_count; i++) {
+        let col = 3 - i;
+        // when looping through all rows, change the row value
+        available.push(['c' + n, 'r' + col]);
+      }
+    }
+    return available;
 
     case 40: //down
-      console.log('down');
-      available = [];
-      axis = "row";
-      // tile.attr({ "data-row": "r2" });
-      for (let n = 0; n < 4; n++) {
-        axis_index = 3;
-        var array = $(".tile[data-col=c" + n + "]");
-        sort_things(array, "data-row", "down");
-        for (let i = 0; i < 4; i++) {
-          slide_tile(array, axis_index, i, axis);
-          axis_index--;
-        }
-        // 4 - tiles.count - how many spaces available
-        let available_count = 4 - array.length;
-        // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
-        for (let i = 0; i < available_count; i++) {
-          // when looping through all rows, change the row value
-          available.push(['c' + n, 'r' + i]);
-        }
+    console.log('down');
+    available = [];
+    axis = "row";
+    // tile.attr({ "data-row": "r2" });
+    for (let n = 0; n < 4; n++) {
+      axis_index = 3;
+      var array = $(".tile[data-col=c" + n + "]");
+      sort_things(array, "data-row", "down");
+      for (let i = 0; i < 4; i++) {
+        slide_tile(array, axis_index, i, axis);
+        axis_index--;
       }
-      return available;
+      // 4 - tiles.count - how many spaces available
+      let available_count = 4 - array.length;
+      // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
+      for (let i = 0; i < available_count; i++) {
+        // when looping through all rows, change the row value
+        available.push(['c' + n, 'r' + i]);
+      }
+    }
+    return available;
 
     case 37: //left
-      console.log('left');
-      available = [];
-      axis = "col";
-      // axis = "data-col";
-      // loop through all rows
-      for (let n = 0; n < 4; n++) {
-        axis_index = 0;
-        array = $(".tile[data-row=r" + n + "]");
-        sort_things(array, "data-col", "left");
-        for (let i = 0; i < array.length; i++) {
-          slide_tile(array, axis_index, i, axis)
-          axis_index++
-        }
-        // 4 - tiles.count - how many spaces available
-        let available_count = 4 - array.length;
-        // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
-        for (let i = 0; i < available_count; i++) {
-          let col = 3 - i;
-          // when looping through all rows, change the row value
-          available.push(['c' + col, 'r' + n]);
-        }
+    console.log('left');
+    available = [];
+    axis = "col";
+    // axis = "data-col";
+    // loop through all rows
+    for (let n = 0; n < 4; n++) {
+      axis_index = 0;
+      array = $(".tile[data-row=r" + n + "]");
+      sort_things(array, "data-col", "left");
+      for (let i = 0; i < array.length; i++) {
+        slide_tile(array, axis_index, i, axis)
+        axis_index++
       }
-      return available;
+      // 4 - tiles.count - how many spaces available
+      let available_count = 4 - array.length;
+      // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
+      for (let i = 0; i < available_count; i++) {
+        let col = 3 - i;
+        // when looping through all rows, change the row value
+        available.push(['c' + col, 'r' + n]);
+      }
+    }
+    return available;
 
     case 39: //right
-      console.log('right');
-      available = [];
-      axis = "col";
-      for (let n = 0; n < 4; n++) {
-        axis_index = 3;
-        array = $(".tile[data-row=r" + n + "]");
-        sort_things(array, "data-col", "right");
-        for (let i = 0; i < array.length; i++) {
-          slide_tile(array, axis_index, i, axis);
-          console.log(axis_index);
-          axis_index--;
-        }
-        // 4 - tiles.count - how many spaces available
-        let available_count = 4 - array.length;
-        // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
-        for (let i = 0; i < available_count; i++) {
-          // when looping through all rows, change the row value
-          available.push(['c' + i, 'r' + n]);
-        }
+    console.log('right');
+    available = [];
+    axis = "col";
+    for (let n = 0; n < 4; n++) {
+      axis_index = 3;
+      array = $(".tile[data-row=r" + n + "]");
+      sort_things(array, "data-col", "right");
+      for (let i = 0; i < array.length; i++) {
+        slide_tile(array, axis_index, i, axis);
+        console.log(axis_index);
+        axis_index--;
       }
-      return available;
+      // 4 - tiles.count - how many spaces available
+      let available_count = 4 - array.length;
+      // array of arrays [['r0', 'c0'], ['r0', 'c1']] and then rando this to get next tile spot
+      for (let i = 0; i < available_count; i++) {
+        // when looping through all rows, change the row value
+        available.push(['c' + i, 'r' + n]);
+      }
+    }
+    return available;
   }
 };
 
 function sort_things(tile_array, sort_by, direction) {
   tile_array = tile_array.sort(function (a, b) {
     var an = a.getAttribute(sort_by),
-        bn = b.getAttribute(sort_by),
-        result;
+    bn = b.getAttribute(sort_by),
+    result;
 
     if(an > bn) {
       result = 1;
@@ -178,15 +178,38 @@ Game.prototype.new_tiles = function(available) {
   let tile = available[Math.floor(Math.random()*available.length)]
 
   let $elem = $("<div/>")
-            .attr({
-                "data-col": tile[0],
-                "data-row": tile[1],
-                "data-val": 2
-              })
-            .addClass("tile")
-            .html("2")
+  .attr({
+    "data-col": tile[0],
+    "data-row": tile[1],
+    "data-val": 2
+  })
+  .addClass("tile")
+  .html("2")
 
   $("#gameboard").append($elem)
+}
+
+Game.prototype.new_game = function() {
+  $(".tile").remove();
+
+  for (let i = 0; i < 2; i++) {
+    let r_num = Math.floor(Math.random() * (3 + 1) + 0);
+    console.log(r_num)
+    let c_num = Math.floor(Math.random() * (3 + 1) + 0);
+    console.log(c_num)
+
+    let $elem = $("<div/>")
+    .attr({
+      "data-col": "c" + c_num,
+      "data-row": "r" + r_num,
+      "data-val": 2
+    })
+    .addClass("tile")
+    .html("2")
+    console.log($elem)
+
+    $("#gameboard").append($elem)
+  };
 }
 
 $(document).ready(function() {
@@ -194,7 +217,11 @@ $(document).ready(function() {
   // Any interactive jQuery functionality
   var game = new Game();
 
-  $('body').keydown(function(event){
+  $("button.new_game_button").bind("click",function() {
+    game.new_game()
+  });
+
+  $("body").keydown(function(event){
     var arrows = [37, 38, 39, 40];
     if (arrows.indexOf(event.which) > -1) {
       var tile = $('.tile');
