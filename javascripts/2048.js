f0f773cc1b7577817450fab4851ea8726de7ca6f
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-08 16:26:16 -0700

    redid display function to show animation but ended up only finishing refactor to work for move right

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -1,4 +1,5 @@
 var Game = function() {
+  this.moves = []
   this.board = {
     "r0c0": null,
     "r1c0": null,
@@ -170,36 +171,37 @@ Game.prototype.moveSingleTileLeft = function(row, col, val) {
 }
 
 Game.prototype.moveSingleTileRight = function(row, col, val) {
+  var new_column = col
   if (col === 'c2') {
     if (!this.board[row + 'c3']) {
-      this.board[row + 'c3'] = val
-      this.board[row + col] = null
+      new_column = 'c3'
     }
   } else if (col === 'c1') {
     if (!this.board[row + 'c2']) {
       if (!this.board[row + 'c3']) {
-        this.board[row + 'c3'] = val
-        this.board[row + col ] = null
+        new_column = 'c3'
       } else {
-      this.board[row + 'c2'] = val
-      this.board[row + col] = null
+        new_column = 'c2'
       }
     }
   }  else if (col === 'c0') {
       if (!this.board[row + 'c1']) {
         if (!this.board[row + 'c2']) {
           if (!this.board[row + 'c3']) {
-            this.board[row + 'c3'] = val
-            this.board[row + col ] = null
+            new_column = 'c3'
           } else {
-            this.board[row + 'c2'] = val
-            this.board[row + col] = null
+            new_column = 'c2'
           }
         } else {
-        this.board[row + 'c1'] = val
-        this.board[row + col] = null
+          new_column = 'c1'
         }
       }
+    }
+    if ( col !== new_column) {
+      this.board[row + new_column] = val
+      this.board[row + col] = null
+      this.moves.push({old_pos: row + col, new_pos: row + new_column})
+      console.log(this.moves)
     }
 }
 
@@ -264,7 +266,7 @@ Game.prototype.moveSingleTileUp = function(row, col, val) {
             this.board[row + col] = null
           } else {
             this.board['r1' + col] = val
-            this.board[row = col] = null
+            this.board[row + col] = null
           }
         } else {
         this.board['r2' + col] = val
@@ -277,31 +279,33 @@ Game.prototype.moveSingleTileUp = function(row, col, val) {
 
 
 Game.prototype.collideRight = function(row, col, val) {
+  var new_col = undefined
   if (col === 'c2') {
     var rightier_col = 'c3'
+
     if (this.board[row + rightier_col] === this.board[row + col]) {
-      this.board[row + rightier_col] = String(Number(val) * 2)
-      this.board[row + col] = null
-      this.score += Number(val)*2
+       new_col = 'c3'
     }
   } else if (col === 'c1') {
     var rightier_col = 'c2'
     if (this.board[row + rightier_col] === this.board[row + col]) {
-      this.board[row + rightier_col] = String(Number(val) * 2)
-      this.board[row + col] = null
-      this.score += Number(val)*2
+      new_col = 'c2'
     }
   }  else if (col === 'c0') {
       var rightier_col = 'c1'
       if (this.board[row + rightier_col] === this.board[row + col]) {
-        this.board[row + rightier_col] = String(Number(val) * 2)
-        this.board[row + col] = null
-        this.score += Number(val)*2
+      new_col = 'c1'
       }
     }
-
+    if (new_col !== undefined) {
+     this.board[row + col] = null
+     this.board[row + new_col] = String(Number(val) * 2)
+     this.score += Number(val)*2
+     this.moves.push({old_pos: row + new_col})
+     this.moves.push({old_pos: row + col, new_pos: row + new_col})
+   }
     $('.score').text(this.score)
-    this.display()
+    // this.display()
 }
 
 Game.prototype.collideLeft = function(row, col, val) {
@@ -347,7 +351,7 @@ Game.prototype.collideDown = function(row, col, val) {
       this.score += Number(val)*2
     }
   }  else if (row === 'r0') {
-      var lower_row = 'c1'
+      var lower_row = 'r1'
       if (this.board[lower_row + col] === this.board[row + col]) {
         this.board[lower_row + col] = String(Number(val) * 2)
         this.board[row + col] = null
@@ -374,7 +378,7 @@ Game.prototype.collideUp = function(row, col, val) {
       this.score += Number(val)*2
     }
   }  else if (row === 'r3') {
-      var lower_col = 'r2'
+      var lower_row = 'r2'
       if (this.board[lower_row + col] === this.board[row + col]) {
         this.board[lower_row + col] = String(Number(val) * 2)
         this.board[row + col] = null
@@ -384,38 +388,69 @@ Game.prototype.collideUp = function(row, col, val) {
     $('.score').text(this.score)
     this.display()
 }
-
 Game.prototype.display = function() {
-  g = this
-  for (var tile in this.board) {
-    var row = /.{2}/.exec(tile)[0]
-    var col = /.{2}$/.exec(tile)[0]
-    var position = row + col
-    if (this.board[tile] != null) {
-      var val = this.board[tile]
-      var currentTile = $('.tile[data-row="' + row + '"][data-col="' + col + '"]')
+  for (var index in this.moves) {
+    var move = this.moves[index]
+    var row = /.{2}/.exec(move.old_pos)[0]
+    var col = /.{2}$/.exec(move.old_pos)[0]
+    var currentTile = $('.tile[data-row="' + row + '"][data-col="' + col + '"]')
+    if (move.new_pos === undefined) {
+      (function(tile) {
+        setTimeout(function() {
+          tile.remove()
+        }, 200 )
+      })(currentTile)
 
-      if (currentTile.length != 0) {
-        currentTile.attr({'data-row': row})
-        currentTile.attr({'data-col': col})
-        currentTile.attr({'data-val': this.board[position]})
-        currentTile.text(this.board[position])
-      } else {
-        var newTileDiv = "<div class='tile' data-row=" + row + " data-col=" + col + " data-val=" + this.board[position] + ">" + this.board[position] + "</div>"
-        var newTile = $("#gameboard").append(newTileDiv);
-      }
-    } else {
-      var currentTile = $('.tile[data-row="' + row + '"][data-col="' + col + '"]')
-      currentTile.remove()
+      continue
     }
+
+    var new_row = /.{2}/.exec(move.new_pos)[0]
+    var new_col = /.{2}$/.exec(move.new_pos)[0]
+    currentTile.attr({'data-row': new_row})
+    currentTile.attr({'data-col': new_col});
+
+    (function(tile, val) {
+      setTimeout(function() {
+        tile.attr({'data-val': val})
+        tile.text(val)
+      } , 200)
+    })(currentTile, this.board[move.new_pos])
   }
+  this.moves = []
 }
+
+// Game.prototype.display = function() {
+//   g = this
+//   for (var tile in this.board) {
+//     var row = /.{2}/.exec(tile)[0]
+//     var col = /.{2}$/.exec(tile)[0]
+//     var position = row + col
+//     if (this.board[tile] != null) {
+//       var val = this.board[tile]
+//       var currentTile = $('.tile[data-row="' + row + '"][data-col="' + col + '"]')
+//
+//       if (currentTile.length != 0) {
+//         currentTile.attr({'data-row': row})
+//         currentTile.attr({'data-col': col})
+//         currentTile.attr({'data-val': this.board[position]})
+//         currentTile.text(this.board[position])
+//       } else {
+//         var newTileDiv = "<div class='tile' data-row=" + row + " data-col=" + col + " data-val=" + this.board[position] + ">" + this.board[position] + "</div>"
+//         var newTile = $("#gameboard").append(newTileDiv);
+//       }
+//     } else {
+//       var currentTile = $('.tile[data-row="' + row + '"][data-col="' + col + '"]')
+//       currentTile.remove()
+//     }
+//   }
+// }
 
 Game.prototype.moveTile = function(tiles, direction) {
   g = this;
   // Game method here
   switch(direction) {
     case 38: //up
+    // var old_positions = $(.'tile')
     this.moveTilesUp(this.occupied())
     this.collideAllUp(this.occupied())
     this.moveTilesUp(this.occupied())
@@ -474,6 +509,15 @@ $(document).ready(function() {
   console.log("ready to go!");
 
   var game = new Game();
+  // var newTileDiv = "<div class='tile' data-row=" + 'r0'+ " data-col=" + 'c0' + " data-val=" + "4" + ">" + "4" + "</div>"
+  // var newTile = $("#gameboard").append(newTileDiv);
+  // var newTileDiv = "<div class='tile' data-row=" + 'r0'+ " data-col=" + 'c1' + " data-val=" + "2" + ">" + "2" + "</div>"
+  // var newTile = $("#gameboard").append(newTileDiv);
+  // var newTileDiv2 = "<div class='tile' data-row=" + 'r0'+ " data-col=" + 'c2' + " data-val=" + "8" + ">" + "8" + "</div>"
+  // var newTile2 = $("#gameboard").append(newTileDiv2);
+  // var newTileDiv3 = "<div class='tile' data-row=" + 'r0'+ " data-col=" + 'c3' + " data-val=" + "8" + ">" + "8" + "</div>"
+  // var newTile3 = $("#gameboard").append(newTileDiv3);
+
   game.display()
   console.log(game.board)
   game.newTile()
@@ -487,6 +531,8 @@ $(document).ready(function() {
     }
     game.display()
     console.log(game.board)
-    game.newTile()
+    setTimeout(function(){
+      game.newTile()
+    }, 200)
   });
 });
