8aaadea1fad6971a5572698a16bc8c6ec5b56fd0
Jillian Boshart <Jilliliz@gmail.com>
2016-07-08 15:52:19 -0700

    added hasLost function. If all spaces are filled, and no moves are possible it alerts the user.

--- javascripts/2048.js
+++ javascripts/2048.js
@@ -2,6 +2,7 @@ var Game = function() {
   this._score = 0
 };
 
+// // // ADD SCORE // // //
 Game.prototype.addScore = function(value_to_add) {
   // add new score to total score
   this._score += parseInt(value_to_add)
@@ -10,6 +11,7 @@ Game.prototype.addScore = function(value_to_add) {
   $('.score').text(this._score)
 }
 
+// // // GET TILES // // //
 Game.prototype.getTiles = function(tile, direction) {
   // set self variable to carry the scope of 'game' throughout method
   var self = this
@@ -39,9 +41,29 @@ Game.prototype.getTiles = function(tile, direction) {
 
     left_right_array.push(r0, r1, r2, r3)
     self.moveTile(left_right_array, direction)
+  } else if (direction === undefined) {
+    var return_array = [],
+        up_down_array = [],
+        left_right_array = []
+
+    c0 = $(tile).filter("[data-col=c0]").toArray(),
+    c1 = $(tile).filter("[data-col=c1]").toArray(),
+    c2 = $(tile).filter("[data-col=c2]").toArray(),
+    c3 = $(tile).filter("[data-col=c3]").toArray()
+
+    r0 = $(tile).filter("[data-row=r0]").toArray(),
+    r1 = $(tile).filter("[data-row=r1]").toArray(),
+    r2 = $(tile).filter("[data-row=r2]").toArray(),
+    r3 = $(tile).filter("[data-row=r3]").toArray()
+
+    up_down_array.push(c0, c1, c2, c3)
+    left_right_array.push(r0, r1, r2, r3)
+    return_array.push(up_down_array, left_right_array)
+    return return_array
   }
 }
 
+// // // ADD TILE // // //
 Game.prototype.addTile = function() {
   var self = this,
       tiles = $('.tile').toArray(),
@@ -60,7 +82,7 @@ Game.prototype.addTile = function() {
     }
   })
 
-  if ($('.tile').length !== 16) {
+  if (tiles.length !== 16) {
     // get random tile location and random value
     var new_loc = vectors[Math.floor(Math.random()*vectors.length)],
         new_val = values[Math.floor(Math.random()*values.length)],
@@ -69,11 +91,54 @@ Game.prototype.addTile = function() {
 
     // Make new div and put it in the tile box
     $('.tile_box').append($("<div>").attr('class', 'tile').attr('data-row', new_row).attr('data-col', new_col).attr('data-val', new_val).text(new_val))
-  } else if ($('.tile').length === 16) {
-    // CALL LOSE?
   }
 }
 
+// // // HAS LOST // // //
+Game.prototype.hasLost = function(tiles) {
+  var return_array = this.getTiles(tiles),
+      up_down = return_array[0],
+      left_right = return_array[1]
+
+  for (var column of up_down) {
+    column.sort(function(a,b) {
+      valA = parseInt($(a).attr("data-row").substring(1))
+      valB = parseInt($(b).attr("data-row").substring(1))
+        return (valA - valB)
+      })
+    }
+
+  for (var row of left_right) {
+    row.sort(function(a,b) {
+      valA = parseInt($(a).attr("data-col").substring(1))
+      valB = parseInt($(b).attr("data-col").substring(1))
+      return (valA - valB)
+    })
+  }
+
+  var all_sorted = []
+  var moves_avail = true
+  all_sorted.push(up_down, left_right)
+
+  $.each(all_sorted, function(i, array) {
+    $.each(array, function(j, column) {
+      $.each(column, function(index, value) {
+
+        // Compair tile values to their neighbors in order
+        let current = $(value).attr('data-val')
+        let next = $(column[index+1]).attr("data-val")
+        if (current === next) {
+          // moves are possible, return false
+          moves_avail = false
+          return moves_avail
+        }
+      })
+    })
+  })
+  return moves_avail
+}
+
+// // // MOVE TILE // // //
 Game.prototype.moveTile = function(array, direction) {
   // set self variable to carry the scope of 'game' throughout method
   var self = this
@@ -274,8 +339,13 @@ $(document).ready(function() {
       var tile = $('.tile');
 
       game.getTiles(tile, event.which);
+      game.addTile()
+    }
 
-      setTimeout(game.addTile, 200)
+    if ($('.tile').length === 16) {
+      if (game.hasLost($('.tile'))) {
+        alert('You lose!')
+      }
     }
   });
 });
